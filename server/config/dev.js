58c4b831cbfa303ea09a6d6231626ec3c7c6fc03
Giorgio S <giorgiosav@me.com>
2022-02-04 14:17:05 -0500

    .gitignore is now working

--- server/config/dev.js
+++
@@ -1,7 +0,0 @@
-//dont commit
-module.exports = {
-  mongoURI:
-    "mongodb+srv://gsav:lD6IzdTUQXXYQYYy@auth.sblve.mongodb.net/Auth?retryWrites=true&w=majority",
-  secretString:
-    "iufhge57984htfd78nw49348urflisgfh794flow7aegff95fht8flgvhe7osr7ifhelghteighi43u23498zxkjhoi#$%",
-};
