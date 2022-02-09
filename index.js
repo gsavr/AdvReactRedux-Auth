6e72dc88371ec790f79b46e6efbc46a20a358694
Giorgio S <giorgiosav@me.com>
2022-02-09 11:44:09 -0500

    added rule for / path in client side

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ app.use(bodyParser.json({ type: "*/*" }));
 //bP parses incoming requests - makes them all into json.
 router(app);
 
+if (process.env.NODE_ENV === "production") {
+  //make sure that express will serve up production assets like main.js / main.css
+  app.use(express.static("client/build"));
+  // Express will serve up index.html if it doesn't recognize the route -- React Router will take care of the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //Server Setup
 const PORT = process.env.PORT || 3090;
 app.listen(PORT);
